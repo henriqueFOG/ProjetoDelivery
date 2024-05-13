d89c1644d7c4e14698100b8d6e941ef3d5223325
Henrique Gonçalves <111437255+henriqueFOG@users.noreply.github.com>
2024-05-13 10:05:01 -0300

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath:'/src/pages',
+  basePath: '/nextjs',
     output:'export',
+    distDir:'dist',
 images: {
     unoptimized: true
   },
